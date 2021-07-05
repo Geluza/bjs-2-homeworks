92c69590da54e830bcdba2c141837571864302f3
Gelyuza Voronina <kgeluza@mail.ru>
2021-07-05 14:49:49 +0400

    Выполнены задачи 1,2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,28 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+   if (arr1.length === arr2.length) {
+    result = arr1.every((item, index) => arr1[item, index] === arr2[item, index]);
+   } else if (arr1.length !== arr2.length) {
+     return false
+   }
 
   return result; // boolean
 }
 
+
+
 function advancedFilter(arr) {
   let resultArr;
+  
+  let arrFilter = arr.filter((item) =>
+    item >= 0 && item % 3 === 0)
+  console.log(arrFilter);
 
-  // Ваш код
+  resultArr = arrFilter.map((item) => {
+   let newArr = item * 10;
+   return newArr;
+  })
+
 
   return resultArr; // array
 }
