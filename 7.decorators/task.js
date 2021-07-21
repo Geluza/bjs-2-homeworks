50898591b6816198120300f2cd800c33828397e3
Gelyuza Voronina <kgeluza@mail.ru>
2021-07-21 23:39:41 +0400

    Решены задачи 1,2,3

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,61 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash)
+    if (idx > -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args);
+      cache.push({hash: `${hash}`, value: `${result}`});
+      if (cache.length > 5) {
+       cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
+//задача2
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
+function debounceDecoratorNew(func, ms) {
+  let checkFunc = true; 
+  return function(...args) {
+   if(checkFunc === false) { 
+   func(...args);
+  }
+  checkFunc = true;
+  setTimeout(() => { 
+  checkFunc = false;
+  }, ms);
+   };
+   }
+   sendSignal = () => console.log("Сигнал отправлен");
+   upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+   setTimeout(upgradedSendSignal); 
+   setTimeout(upgradedSendSignal, 300); 
+   setTimeout(upgradedSendSignal, 900); 
+   setTimeout(upgradedSendSignal, 1200); 
+   setTimeout(upgradedSendSignal, 2300); 
+   setTimeout(upgradedSendSignal, 4400); 
+   setTimeout(upgradedSendSignal, 4500); 
+  
+   //задача 3
 function debounceDecorator2(func) {
-  // Ваш код
+  let checkFunc = true;
+  wrapper.count = 0;
+  function wrapper(...args) {
+     if(checkFunc === false) { 
+     func(...args); 
+    } 
+    checkFunc = true;
+    setTimeout(() => { 
+    checkFunc = false;
+    }, ms);
+    wrapper.count += 1;
+  }
+  return wrapper;
 }
